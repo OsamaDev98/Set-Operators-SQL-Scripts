d3c9955349a40e3eca15c1efad7b7af5904c781c
osama <osamadev921@gmail.com>
2025-10-21 03:16:33 +0300

    [ADD] Add set operators sql scripts.

---
+++ Set_Operators.sql
@@ -0,0 +1,109 @@
+USE SalesDB;
+
+-- Combine the data from employees and customers into one table.
+SELECT
+	FirstName,
+	LastName
+FROM
+	Sales.Employees
+
+UNION
+
+SELECT
+	FirstName,
+	LastName
+FROM
+	Sales.Customers;
+
+/*
+	Combine the data from employees and customers
+	into one table, including duplicate.
+*/
+SELECT
+	FirstName,
+	LastName
+FROM
+	Sales.Employees
+
+UNION ALL
+
+SELECT
+	FirstName,
+	LastName
+FROM
+	Sales.Customers;
+
+-- Find employees who are not customers at the same time.
+SELECT
+	FirstName,
+	LastName
+FROM
+	Sales.Employees
+
+EXCEPT
+
+SELECT
+	FirstName,
+	LastName
+FROM
+	Sales.Customers;
+
+-- Find the employees, who are also customers.
+SELECT
+	FirstName,
+	LastName
+FROM
+	Sales.Employees
+
+INTERSECT
+
+SELECT
+	FirstName,
+	LastName
+FROM
+	Sales.Customers;
+
+/*
+	Orders are stored in separate tables (Orders and OrdersArchive)
+	combine all orders into one report withjout duplicates.
+*/
+SELECT
+	'Orders' AS SourceTable
+	,[OrderID]
+	,[ProductID]
+	,[CustomerID]
+	,[SalesPersonID]
+	,[OrderDate]
+	,[ShipDate]
+	,[OrderStatus]
+	,[ShipAddress]
+	,[BillAddress]
+	,[Quantity]
+	,[Sales]
+	,[CreationTime]
+FROM
+	Sales.Orders
+
+UNION
+
+SELECT
+	'OrdersArchive' AS SourceTable
+	,[OrderID]
+	,[ProductID]
+	,[CustomerID]
+	,[SalesPersonID]
+	,[OrderDate]
+	,[ShipDate]
+	,[OrderStatus]
+	,[ShipAddress]
+	,[BillAddress]
+	,[Quantity]
+	,[Sales]
+	,[CreationTime]
+FROM
+	Sales.OrdersArchive
+ORDER BY
+	OrderID;
+
+
+
